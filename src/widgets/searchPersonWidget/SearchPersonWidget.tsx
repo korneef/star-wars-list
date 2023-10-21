269e611443965dface82620303a4d41f50c7b8f5
Valentin Korneev <v.korneef@gmail.com>
2023-10-21 14:00:46 +0600

    fix bug with search in search input, add function for hide list component if user make click out of the list or input

--- src/widgets/searchPersonWidget/SearchPersonWidget.tsx
+++ src/widgets/searchPersonWidget/SearchPersonWidget.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import useGetData from '../../customHooks/useGetData';
 import { Tpeople } from '../../models/peopleModel';
 import './SearchPersonWidget.scss';
@@ -15,21 +15,32 @@ function SearchPersonWidget() {
       ? ''
       : `https://swapi.dev/api/people/?search=${ personSearchStr.searchStr }&page=${ personSearchStr.pageStr }`
   );
-  const [isFocus, setIsFocus] = useState(false)
+  const [isFocus, setIsFocus] = useState(false);
+  const searchPersonWidgetRef = useRef<HTMLDivElement | null>(null);
 
   const handleChange = (value: string) => {
     setPersonSearchStr(prevState => {
-      return {...prevState, searchStr: value}
+      return {pageStr: '1', searchStr: value}
     });
   }
+
+  useEffect(() => {
+      if (!isFocus) return;
+
+      const checkIsFocus = (evt: MouseEvent) => {
+        if (!searchPersonWidgetRef.current) return;
+        if (!searchPersonWidgetRef.current.contains(evt.target as Node)) setIsFocus(false);
+      };
+
+      document.addEventListener('mousedown', checkIsFocus)
+      return () => document.removeEventListener('mousedown', checkIsFocus);
+    },
+    [isFocus, searchPersonWidgetRef])
+
   const handleFocus = (value: boolean) => {
     setIsFocus(value);
   }
 
-  const handleClose = () => {
-    setIsFocus(false);
-  }
-  const className = 'search-person-widget';
   const searchPageChange = (url: string | null) => {
     if (url === null) return;
     const match = url.match(/page=(\d+)/);
@@ -41,6 +52,9 @@ function SearchPersonWidget() {
       })
     }
   }
+
+  const className = 'search-person-widget';
+
   const getPersonId = (url: string | null): string => {
     if (url === null) return '';
     const match = url.match(/people\/(\d+)\/?$/);
@@ -48,7 +62,10 @@ function SearchPersonWidget() {
     return ''
   }
   return (
-    <div className={ className }>
+    <div
+      className={ className }
+      ref={ searchPersonWidgetRef }
+    >
       <input
         className={ `${ className }__input` }
         value={ personSearchStr.searchStr }
@@ -59,9 +76,6 @@ function SearchPersonWidget() {
       { isFocus && data && <div
         className={ `${ className }__persons-list-wrapper` }
       >
-        <div className={ `${ className }__persons-button-wrapper` }>
-          <button onClick={ handleClose }>x</button>
-        </div>
         <ul className={ `${ className }__persons-list` }>
           { data?.results && data.results.map(item => <li
             key={ item.name }
@@ -71,7 +85,7 @@ function SearchPersonWidget() {
                     to={ `/peoples/${ getPersonId(item.url) }` }>{ item.name }</Link> }
           </li>) }
         </ul>
-        <div className={ `${ className }__persons-button-wrapper` }>{ data.previous &&
+        {(data.next || data.previous) && <div className={ `${ className }__persons-button-wrapper` }>{ data.previous &&
           <Button
             onClick={ () => searchPageChange(data.previous) }
             disabled={ isLoading }
@@ -85,7 +99,7 @@ function SearchPersonWidget() {
             >
               Вперед
             </Button> }
-        </div>
+        </div> }
       </div> }
     </div>
   );
